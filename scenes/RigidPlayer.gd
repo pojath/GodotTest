c594bda41bdd5bfd5ab39939fbdc047af3d9c263
pojath <pojath@gmail.com>
2023-11-01 23:56:09 -1000

    lots of changes

--- scenes/RigidPlayer.gd
+++ scenes/RigidPlayer.gd
@@ -6,54 +6,78 @@ var velocity = Vector2.ZERO
 const SPEED = 300.0
 var spawnPointRef = null
 signal testingSignal()
+signal enterSignal()
+signal collisionSignal()
 var weDropped
 var line2D = null
-
+var controls = true
+var readyToDrop
+var fruitID
 
 @onready var myName = self.name
 
 func _ready():
-	print (myName)
+	readyToDrop = false
 	weDropped = false
-	spawnPointRef = get_parent().get_parent()
-	connect("testingSignal", Callable(spawnPointRef, '_on_my_custom_signal'))
+	spawnPointRef = get_parent()
+	connect("testingSignal", Callable(spawnPointRef, '_custom_signal'))
+	connect("enterSignal", Callable(spawnPointRef, '_fruit_was_dropped'))
+	connect("collisionSignal", Callable(spawnPointRef, '_collision_manager'))
 	
-	var rightSpawnPoint = get_node('../../../StaticBody2D2/RightSpawnPoint')
-	var leftSpawnPoint = get_node('../../../StaticBody2D2/LeftSpawnPoint')
+	var rightSpawnPoint = get_node('../../StaticBody2D2/RightSpawnPoint')
+	var leftSpawnPoint = get_node('../../StaticBody2D2/LeftSpawnPoint')
 	line2D = get_node('./Line2D')
 
 	left_position = Vector2(leftSpawnPoint.global_position.x, leftSpawnPoint.global_position.y)
 	right_position = Vector2(rightSpawnPoint.global_position.x, rightSpawnPoint.global_position.y)
 
+func _set_name(newName, incrementer):
+	self.name = "Fruit" + str(newName) + str(incrementer)
+	fruitID = "Fruit" + str(newName)
+	
+func _not_ready_to_drop():
+	readyToDrop = false
+
+func _get_fruit_ID():
+	return fruitID
+
+func _ready_to_drop():
+	readyToDrop = true
+
+func _input(event):
+	if event.is_action_pressed("ui_accept") && readyToDrop:
+		enterSignal.emit()
+		weDropped = true
+		_delete_line()
+		
+	if event.is_action_pressed("ui_up"):
+#		collisionSignal.emit(get_parent())
+		pass
 
 func _physics_process(delta):
-	if !weDropped:
+	if readyToDrop:
 		leftyrightymovement()
-	else:
+	if weDropped:
 		velocity = Vector2.ZERO
 		gravity_scale = 1
 
-	if Input.is_action_pressed("ui_accept"):
-		weDropped = true
-		spawnPointRef.playerInfo(weDropped)
-		if is_instance_valid(line2D):
-			line2D.queue_free()
-		
 	var collision = move_and_collide(velocity * delta)
 	
 	if collision:
+		var fruitName = collision.get_collider()
+		var startLetter = str(fruitName).left(1)
+		
 
-		var fruitName = collision.get_collider().name
-		var fruitNode = collision.get_collider()
-		
-		
 		if collision.get_collider_shape().name == "CollisionShapeLeft":
 			global_position = right_position
 		elif collision.get_collider_shape().name == "CollisionShapeRight":
 			global_position = left_position
-		elif myName == fruitName:
-			fruitCombiner(collision, fruitName, fruitNode)
-			gravity_scale = 0
+		elif startLetter != "S":
+			var enemyFruitID = collision.get_collider().get("fruitID")
+			print("enemyfruitid is: ",enemyFruitID)
+			if self.fruitID == enemyFruitID:
+				fruitCombiner(collision, fruitName)
+				gravity_scale = 0
 #
 func leftyrightymovement():
 	var direction = Input.get_axis("ui_left", "ui_right")
@@ -61,27 +85,33 @@ func leftyrightymovement():
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-func fruitCombiner(collision, fruitName, fruitNode):
+
+
+func _find_fruit_id(blah):
+	var fruity = blah.left(6)
+	return fruity
+
+func fruitCombiner(fruitCollision, fruitName):
 	var pos1 = self.global_position
-	var pos2 = collision.get_collider().global_position
+	var pos2 = fruitCollision.get_collider().global_position
 	var pos3 = Vector2()
 	pos3 = (pos1 + pos2)/2
-	var deleteFruit = fruitNode
+
 	var newFruit = str(fruitName)
 	
 	#get the length of the string
 	var newFruitLength = newFruit.length() - 1
-	
 	if newFruitLength > 0:
+		
 		var minFruit = 1
-		var maxFruit = 5
+		var maxFruit = 9
 	
 		#get all the characters except for the last one
 		var fruitTemplateName = newFruit.left(newFruitLength)
-		
+
 		#get the last character so we can add 1 to it
 		var lastCharacter = newFruit[newFruitLength]
+
 		
 		#add 1 to it
 		var newChar = int(lastCharacter) + 1
@@ -89,34 +119,23 @@ func fruitCombiner(collision, fruitName, fruitNode):
 		#clamp the value
 		var clampedFruit = clamp(newChar, minFruit, maxFruit)
 		
-		#combine it all
-		var newerfruit = fruitTemplateName + str(clampedFruit)
-		print("the fruit to spawn is: ", newerfruit)
-		
 		#feed SpawnPoint the # to spawn
-		if int(lastCharacter) != 5:
-			fruitDeleter(deleteFruit)
-			testingSignal.emit(clampedFruit, pos3)
-		
-func fruitDeleter(fruitNode):
-	print("fruit deleter")
-	if is_instance_valid(fruitNode):
-		print("fruit deleter is working")
-		fruitNode.queue_free()
-		fkYourself()
+		if int(lastCharacter) != 9:
+			print("emitting signal")
+			var selfFruit = self
+			collisionSignal.emit(selfFruit, fruitName, pos3, fruitID)
 
-	#measure the distance between the two objects
-	#figure out which one to instantiate by getting the name and + 1 it.
-	#instantiate a new one
-	
 func weSpawned():
 	weDropped = true
+	_delete_line()
+
+func _delete_line():
+	controls = false
 	if is_instance_valid(line2D):
 		line2D.queue_free()
-		
-func fkYourself():
-	print("go fk yourself")
-	if is_instance_valid(self):
-		print("go fk yourself is working")
-		get_parent().queue_free()
-		queue_free()	
+
+func _hide_line():
+	line2D.visible = !line2D.visible
+	
+
+	
--- scripts/SpawnPoint.gd
+++ scripts/SpawnPoint.gd
@@ -9,55 +9,138 @@ var elapsedTime = 0.0
 var fruitList = []
 var instance = null
 var random_fruit_index = null
+var nextFruit = null
+var fruitLoaded
+var fruitToChamber = null
+var isLoaded
+var fruitInChamber = []
+var thefruit = null
+var sup2
+var fruitDict = {}
+var fruitKey = 1
+var increments = 0
 
-signal myCustomSignal
+
+#signal myCustomSignal
 
 func _ready():
-	pass
+	#load the packed scene
+	var scene_instance = preload("res://FruitTemplates/Fruit1.tscn")
 
-func _process(delta):
-	if hasDropped:
-		elapsedTime += delta
+	fruitList.append(scene_instance)
+	scene_instance = preload("res://FruitTemplates/Fruit2.tscn")
+
+	fruitList.append(scene_instance)
+	scene_instance = preload("res://FruitTemplates/Fruit3.tscn")
+
+	fruitList.append(scene_instance)
+	scene_instance = preload("res://FruitTemplates/Fruit4.tscn")
+
+	fruitList.append(scene_instance)
+	scene_instance = preload("res://FruitTemplates/Fruit5.tscn")
+
+	fruitList.append(scene_instance)
+	scene_instance = preload("res://FruitTemplates/Fruit6.tscn")
+
+	fruitList.append(scene_instance)
+	scene_instance = preload("res://FruitTemplates/Fruit7.tscn")
+
+	fruitList.append(scene_instance)
+	scene_instance = preload("res://FruitTemplates/Fruit8.tscn")
+
+	fruitList.append(scene_instance)
+	scene_instance = preload("res://FruitTemplates/Fruit9.tscn")
+
+	fruitList.append(scene_instance)
 	
-	#if enough time has passed and there isn't an object already there
-	check_cooldown()
-#	spawnFunction()
+	#pick a random fruit from fruitlist
+	_load_the_load_zone()
+	_move_to_spawn()
+	_load_the_load_zone()
+	
 
-func check_cooldown():
-	if elapsedTime >= spawnCooldown && hasDropped:
-		#this is where I spawn
-		spawnPlayer()
-		hasDropped = false
+func _fruit_was_dropped():
+	#this is the enter signal function
+	$Timer.start()
+	
+
 
 func playerInfo(blah):
 	hasDropped = blah
 
 
-func spawnPlayer():
-	#load the packed scene
-	var scene_instance = load("res://FruitTemplates/Fruit1.tscn")
-	fruitList.append(scene_instance)
-	scene_instance = load("res://FruitTemplates/Fruit2.tscn")
-	fruitList.append(scene_instance)
-	scene_instance = load("res://FruitTemplates/Fruit3.tscn")
-	fruitList.append(scene_instance)
-	scene_instance = load("res://FruitTemplates/Fruit4.tscn")
-	fruitList.append(scene_instance)
-	scene_instance = load("res://FruitTemplates/Fruit5.tscn")
-	fruitList.append(scene_instance)
-	
-	random_fruit_index = randi_range(0, fruitList.size()-1)
-	
-	instance = fruitList[random_fruit_index].instantiate()
-	add_child(instance)
+#the main spawner...take the array fruitInChamber and instantiate the 0 index.
+func _spawnPlayer():
 	elapsedTime = 0
 
-
-
-func _on_my_custom_signal(fruitToSpawn, pos3):
+#this signal is sent from rigid player so we can combine fruits...
+func _custom_signal(fruitToSpawn, pos3):
 	print("this is a spawn point test signal, we're spawning #: ", fruitToSpawn)
+	_fruit_combiner(fruitToSpawn, pos3)
+	
+func _find_next_fruit():
+	var maxFruitIndex = 4
+	random_fruit_index = randi_range(0, fruitList.size()-1)
+	random_fruit_index = clamp(random_fruit_index, 0, maxFruitIndex)
+
+func _fruit_maker():
+	nextFruit = fruitList[random_fruit_index].instantiate()
+	nextFruit._set_name(random_fruit_index + 1, increments)
+	increments += 1
+	add_child(nextFruit)
+
+
+func _move_to_load_zone():
+	nextFruit.global_position = %LoadingZone.global_position
+	thefruit = nextFruit
+	thefruit._not_ready_to_drop()
+
+func _move_to_spawn():
+	nextFruit.global_position = self.global_position
+	thefruit._ready_to_drop()
+	
+	#connect this to a timer
+	
+#	_load_the_load_zone()
+	
+func _load_the_load_zone():
+	_find_next_fruit()
+	_fruit_maker()
+	_move_to_load_zone()
+
+func _fruit_combiner(fruitToSpawn, pos3):
+	fruitDict.clear()
+	if fruitToSpawn >8:
+		pass
+	print("doing fruit combiner")
+	#this is for combining fruits...
 	instance = fruitList[fruitToSpawn-1].instantiate()
+	instance._set_name(fruitToSpawn, increments)
+	increments += 1
 	add_child(instance)
 	instance.global_position = pos3
-	var blah = instance.get_child(0)
+	var blah = instance
+	blah._not_ready_to_drop()
 	blah.weSpawned()
+
+func _on_timer_timeout():
+	_move_to_spawn()
+	_load_the_load_zone()
+
+func _collision_manager(myFruit, theirFruit, pos3, fruitID):
+	print("my fruit is: ", myFruit)
+	print("their fruit is: ", theirFruit)
+	fruitDict[myFruit] = theirFruit
+	print(fruitDict)
+	for key in fruitDict:
+		if key != null:
+			if is_instance_valid(myFruit):
+				myFruit.queue_free()
+			if is_instance_valid(theirFruit):
+				theirFruit.queue_free()
+			var number = int(fruitID.right(1)) + 1
+			_fruit_combiner(number, pos3)
+			
+
+	
+	
